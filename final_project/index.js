56c4f3e4e184052e41a751d6447bce92d0ca4c2b
DJ-22 <jaindaksh006@gmail.com>
2025-07-15 19:10:08 -0400

    feat: allow logged-in users to add or modify book reviews

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    if (req.session && req.session.authorizatoin) {
+    if (req.session && req.session.authorization) {
         const token = req.session.authorization.accessToken;
         jwt.verify(token, "access", (err, user) => {
             if (!err) {
@@ -24,7 +24,6 @@ app.use("/customer/auth/*", function auth(req,res,next){
     } else {
         return res.status(403).json({ message: "User not logged in" });
     }
-
 });
  
 const PORT =5000;
